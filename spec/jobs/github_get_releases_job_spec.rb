139a6320f0020dbe209bc943d580bfbd57c9d1d2
Louis Ramos <louisramosdev@gmail.com>
2023-10-03 20:18:38 +0200

    Stubbed jobs test

--- spec/jobs/github_get_releases_job_spec.rb
+++ spec/jobs/github_get_releases_job_spec.rb
@@ -1,7 +1,78 @@
 require "rails_helper"
 
+module GithubGetReleasesJobSpec
+  class Response
+    attr_reader :body
+
+    def initialize
+      @body = [{
+        "id" => 1,
+        "user" => {
+          "id" => 1
+        },
+        "content" => "+1"
+      }].to_json
+    end
+  end
+end
+
+class Release
+  attr_reader :id, :name, :tag_name, :body, :html_url, :created_at, :author
+
+  def initialize
+    @id = 1
+    @name = "a"
+    @tag_name = "a"
+    @body = "a"
+    @html_url = "a"
+    @created_at = Time.current
+    @author = User.new
+  end
+end
+
+class Starred
+  attr_reader :id, :name, :full_name, :description, :language, :stargazers_count, :forks_count, :html_url,
+    :pushed_at, :owner
+
+  def initialize
+    @id = 1
+    @name = "a"
+    @full_name = "a"
+    @description = "a"
+    @language = "a"
+    @stargazers_count = 1
+    @forks_count = 1
+    @html_url = "a"
+    @pushed_at = Time.current
+    @owner = User.new
+  end
+end
+
+class User
+  attr_reader :id, :login, :type, :avatar_url, :html_url, :name, :bio, :location
+
+  def initialize
+    @id = 1
+    @login = "a"
+    @type = "a"
+    @avatar_url = "a"
+    @html_url = "a"
+    @name = "a"
+    @nio = "a"
+    @location = "a"
+  end
+end
+
 RSpec.describe GithubGetReleasesJob do
+  let!(:client) { Octokit::Client.new(access_token: Rails.application.credentials.github_token) }
+
   before do
+    allow(Octokit::Client).to receive_messages(new: client)
+    allow(client).to receive_messages(starred: [Starred.new])
+    allow(client).to receive_messages(releases: [Release.new])
+    allow(client).to receive_messages(user: User.new)
+    allow(HTTParty).to receive_messages(get: GithubGetReleasesJobSpec::Response.new)
+
     described_class.perform_now({ reaction_limit: 1 })
     described_class.perform_now({ reaction_limit: 1 })
   end
--- spec/jobs/github_handle_reaction_job_spec.rb
+++ spec/jobs/github_handle_reaction_job_spec.rb
@@ -1,26 +1,39 @@
 require "rails_helper"
 
+module GithubHandleReactionJobSpec
+  class Response
+    attr_reader :code
+
+    def initialize
+      @code = 204
+    end
+  end
+end
+
 RSpec.describe GithubHandleReactionJob do
   let!(:repository) { create(:github_repository) }
-  let!(:fake_repository) { create(:github_repository, :is_fake) }
   let!(:release) { create(:github_release) }
   let!(:reaction) { create(:github_reaction) }
 
-  before do
-    described_class.perform_now(repository, release, reaction, "create")
-  end
-
   it "POST a new reaction on github" do
+    allow(HTTParty).to receive_messages(post: { "id" => 2 })
     described_class.perform_now(repository, release, reaction, "create")
+
     expect(reaction.gid).not_to eq 1
   end
 
   it "destroys the temporary instance if the POST failed" do
-    described_class.perform_now(fake_repository, release, reaction, "create")
+    allow(HTTParty).to receive_messages(post: { "id" => nil })
+    described_class.perform_now(repository, release, reaction, "create")
+
     expect(GithubReaction.count).to eq 0
   end
 
   it "DELETE a reaction from github" do
+    allow(HTTParty).to receive_messages(post: { "id" => 2 })
+    allow(HTTParty).to receive_messages(delete: GithubHandleReactionJobSpec::Response.new)
+
+    described_class.perform_now(repository, release, reaction, "create")
     response = described_class.perform_now(repository, release, reaction.gid, "delete")
     expect(response.code).to eq 204
   end
