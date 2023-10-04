a0cd24c7090057e9222795aebde7a94418f74020
Louis Ramos <louisramosdev@gmail.com>
2023-10-04 22:10:21 +0200

    Updated cron jobs class name

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -6,19 +6,19 @@ Rails.application.configure do
   config.good_job.cron = {
     github_get_releases_morning: {
       cron: "0 5 * * *",
-      class: "GithubGetReleasesJob",
+      class: "Github::GetReleasesJob",
       kwargs: { starred_limit: 60, release_limit: 3 },
       description: "Fetching Github for new releases"
     },
     github_get_releases_noon: {
       cron: "0 10 * * *",
-      class: "GithubGetReleasesJob",
+      class: "Github::GetReleasesJob",
       kwargs: { starred_limit: 60, release_limit: 3 },
       description: "Fetching Github for new releases"
     },
     github_get_releases_afternoon: {
       cron: "0 15 * * *",
-      class: "GithubGetReleasesJob",
+      class: "Github::GetReleasesJob",
       kwargs: { starred_limit: 60, release_limit: 3 },
       description: "Fetching Github for new releases"
     }
