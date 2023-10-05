a3c941cd9f2210f4432b8e54731dd2c2b926b3a8
Louis Ramos <louisramosdev@gmail.com>
2023-10-05 21:35:22 +0200

    Updated erronous class reference in old migration file

--- db/migrate/20230929200903_add_data_to_github_repositories.rb
+++ db/migrate/20230929200903_add_data_to_github_repositories.rb
@@ -5,7 +5,8 @@ class AddDataToGithubRepositories < ActiveRecord::Migration[7.0]
     add_column :github_repositories, :pushed_at, :datetime
     add_column :github_repositories, :html_url, :string
 
-    GithubRepository.update_all(stargazers_count: 0, forks_count: 0, pushed_at: 1.month.ago, html_url: "www.github.com")
+    Github::Repository
+      .update_all(stargazers_count: 0, forks_count: 0, pushed_at: 1.month.ago, html_url: "www.github.com")
 
     change_column_null :github_repositories, :stargazers_count, false
     change_column_null :github_repositories, :forks_count, false
