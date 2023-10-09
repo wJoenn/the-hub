c3fddca8c24184cc7fe08675b0f3f3a1de5faec3
Louis Ramos <louisramosdev@gmail.com>
2023-10-09 19:34:17 +0200

    Increase freqency of CRON jobs

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -4,41 +4,17 @@ Rails.application.configure do
   config.good_job.enable_cron = true
 
   config.good_job.cron = {
-    github_get_comments_morning: {
-      cron: "0 4 * * *",
+    github_get_comments: {
+      cron: "/30 * * * *",
       class: "Github::GetCommentsJob",
       kwargs: { notification_limit: 15, issue_comment_limit: 100, reaction_limit: 100 },
-      description: "Fetching Github for new issue comments"
+      description: "Fetching Github for new issue comments every 30 minutes"
     },
-    github_get_releases_morning: {
-      cron: "0 5 * * *",
+    github_get_releases: {
+      cron: "0 /2 * * *",
       class: "Github::GetReleasesJob",
       kwargs: { starred_limit: 60, release_limit: 3 },
-      description: "Fetching Github for new releases"
-    },
-    github_get_comments_noon: {
-      cron: "0 9 * * *",
-      class: "Github::GetCommentsJob",
-      kwargs: { notification_limit: 5, issue_comment_limit: 100, reaction_limit: 100 },
-      description: "Fetching Github for new issue comments"
-    },
-    github_get_releases_noon: {
-      cron: "0 10 * * *",
-      class: "Github::GetReleasesJob",
-      kwargs: { starred_limit: 60, release_limit: 3 },
-      description: "Fetching Github for new releases"
-    },
-    github_get_comments_afternoon: {
-      cron: "0 14 * * *",
-      class: "Github::GetCommentsJob",
-      kwargs: { notification_limit: 5, issue_comment_limit: 100, reaction_limit: 100 },
-      description: "Fetching Github for new issue comments"
-    },
-    github_get_releases_afternoon: {
-      cron: "0 15 * * *",
-      class: "Github::GetReleasesJob",
-      kwargs: { starred_limit: 60, release_limit: 3 },
-      description: "Fetching Github for new releases"
+      description: "Fetching Github for new releases every 2 hours"
     }
   }
 end
